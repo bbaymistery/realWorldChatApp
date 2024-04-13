92d363e06ac04c56afdc21770ca58f8d544e70c9
bbaymistery <elgun.ezmemmedov@mail.ru>
2024-04-13 14:40:59 +0400

    AddingRedux And ConcatDashboardSection

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
+import { Provider as ReduxProvider } from 'react-redux';
+import { store } from './redux/store';
 // contexts
 import SettingsProvider from "./contexts/SettingsContext";
 
@@ -13,16 +14,15 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <HelmetProvider>
-      <SettingsProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </SettingsProvider>
+      <ReduxProvider store={store}>
+        <SettingsProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </SettingsProvider>
+      </ReduxProvider>
     </HelmetProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
---
+++ src/sections/Dashboard/ContactDashboardSection.js
@@ -0,0 +1,160 @@
+import React, { useState } from "react";
+import { useTheme } from "@mui/material/styles";
+import { Avatar, Box, Button, Divider, IconButton, Stack, Typography, Slide, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, } from "@mui/material";
+import { faker } from "@faker-js/faker";
+import { Bell, CaretRight, Phone, Prohibit, Star, Trash, VideoCamera, X, } from "phosphor-react";
+import useResponsive from "../../hooks/useResponsive";
+import AntSwitch from "../../components/AntSwitchStyled";
+import { ToggleSidebar } from "../../redux/slices/app";
+import { useDispatch } from "react-redux";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
+const BlockDialog = ({ open, handleClose }) => {
+    return (
+        <Dialog open={open} TransitionComponent={Transition} keepMounted onClose={handleClose} aria-describedby="alert-dialog-slide-description"  >
+            <DialogTitle>Block this contact</DialogTitle>
+            <DialogContent>
+                <DialogContentText id="alert-dialog-slide-description">
+                    Are you sure you want to block this Contact?
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+                <Button onClick={handleClose}>Yes</Button>
+            </DialogActions>
+        </Dialog>
+    );
+};
+
+const DeleteChatDialog = ({ open, handleClose }) => {
+    return (
+        <Dialog open={open} TransitionComponent={Transition} keepMounted onClose={handleClose} aria-describedby="alert-dialog-slide-description" >
+            <DialogTitle>Delete this chat</DialogTitle>
+            <DialogContent>
+                <DialogContentText id="alert-dialog-slide-description">
+                    Are you sure you want to delete this chat?
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+                <Button onClick={handleClose}>Yes</Button>
+            </DialogActions>
+        </Dialog>
+    );
+};
+
+const ContactDashboardSection = () => {
+    const theme = useTheme();
+    const dispatch = useDispatch()
+    const isDesktop = useResponsive("up", "md");
+    const [openBlock, setOpenBlock] = useState(false);
+    const [openDelete, setOpenDelete] = useState(false);
+    const handleCloseBlock = () => setOpenBlock(false);
+    const handleCloseDelete = () => setOpenDelete(false);
+
+    const bgBox = theme.palette.mode === "light" ? "#F8FAFF" : theme.palette.background
+    return (
+        <Box sx={{ width: !isDesktop ? "100vw" : 320, maxHeight: "100vh" }}>
+            <Stack sx={{ height: "100%" }}>
+                <Box sx={{ boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)", width: "100%", backgroundColor: bgBox }} >
+                    <Stack sx={{ height: "100%", p: 2 }} direction="row" alignItems={"center"} justifyContent="space-between" spacing={3}>
+                        <Typography variant="subtitle2">Contact Info</Typography>
+                        <IconButton onClick={() => { dispatch(ToggleSidebar()); }} >  <X /> </IconButton>
+                    </Stack>
+                </Box>
+                <Stack sx={{
+                    height: "100%",
+                    position: "relative",
+                    flexGrow: 1,
+                    overflowY: "scroll",
+
+                    '&::-webkit-scrollbar': {
+                        width: '4px',  // Width of the scrollbar
+                    },
+                    '&::-webkit-scrollbar-track': {
+                        background: 'transparent', // Color of the scrollbar track
+                    },
+                    '&::-webkit-scrollbar-thumb': {
+                        background: '#555', // Color of the scrollbar thumb
+                        borderRadius: '4px', // Border radius of the scrollbar thumb
+                    },
+                    '&::-webkit-scrollbar-thumb:hover': {
+                        background: '#888', // Color of the scrollbar thumb on hover
+                    },
+                }} p={2} spacing={3}   >
+                    <Stack alignItems="center" direction="row" spacing={2}>
+                        <Avatar src={faker.image.avatar()} sx={{ height: 64, width: 64 }} />
+                        <Stack spacing={0.5}>
+                            <Typography variant="article" fontWeight={600}> {"Elgun"}</Typography>
+                            <Typography variant="body2" fontWeight={500}>{"+91 62543 28 739"} </Typography>
+                        </Stack>
+                    </Stack>
+                    <Stack direction="row" alignItems="center" justifyContent={"space-evenly"}  >
+                        <Stack alignItems={"center"} spacing={1}>
+                            <IconButton><Phone /></IconButton>
+                            <Typography variant="overline">Voice</Typography>
+                        </Stack>
+                        <Stack alignItems={"center"} spacing={1}>
+                            <IconButton><VideoCamera /></IconButton>
+                            <Typography variant="overline">Video</Typography>
+                        </Stack>
+                    </Stack>
+                    <Divider />
+                    <Stack spacing={0.5}>
+                        <Typography variant="article" fontWeight={600}>  About </Typography>
+                        <Typography variant="body2" fontWeight={500}>  {"I am Developer"}  </Typography>
+                    </Stack>
+                    <Divider />
+                    <Stack direction="row" alignItems="center" justifyContent={"space-between"}    >
+                        <Typography variant="subtitle2">Media, Links & Docs</Typography>
+                        <Button endIcon={<CaretRight />} > 401 </Button>
+                    </Stack>
+                    <Stack direction={"row"} alignItems="center" spacing={2}>
+                        {[1, 2, 3].map((el, i) => (<Box key={i}>  <img src={faker.image.city()} alt={faker.internet.userName()} />   </Box>))}
+                    </Stack>
+                    <Divider />
+                    <Stack direction="row" alignItems="center" justifyContent={"space-between"}>
+                        <Stack direction="row" alignItems="center" spacing={2}>
+                            <Star size={21} />
+                            <Typography variant="subtitle2">Starred Messages</Typography>
+                        </Stack>
+                        <IconButton><CaretRight /></IconButton>
+                    </Stack>
+                    <Divider />
+                    <Stack direction="row" alignItems="center" justifyContent={"space-between"}  >
+                        <Stack direction="row" alignItems="center" spacing={2}>
+                            <Bell size={21} />
+                            <Typography variant="subtitle2">Mute Notifications</Typography>
+                        </Stack>
+                        <AntSwitch />
+                    </Stack>
+                    <Divider />
+                    <Typography variant="body2">1 group in common</Typography>
+                    <Stack direction="row" alignItems={"center"} spacing={2}>
+                        <Avatar src={faker.image.imageUrl()} alt={faker.name.fullName()} />
+                        <Stack direction="column" spacing={0.5}>
+                            <Typography variant="subtitle2">Camel’s Gang</Typography>
+                            <Typography variant="caption">   Owl, Parrot, Rabbit , You      </Typography>
+                        </Stack>
+                    </Stack>
+                    <Divider />
+                    <Stack direction="row" alignItems={"center"} spacing={2}>
+                        <Button onClick={() => { setOpenBlock(true); }} fullWidth startIcon={<Prohibit />} variant="outlined">
+                            Block
+                        </Button>
+                        <Button onClick={() => { setOpenDelete(true); }} fullWidth startIcon={<Trash />} variant="outlined">
+                            Delete
+                        </Button>
+                    </Stack>
+                </Stack>
+            </Stack>
+            {openBlock && <BlockDialog open={openBlock} handleClose={handleCloseBlock} />}
+            {openDelete && <DeleteChatDialog open={openDelete} handleClose={handleCloseDelete} />}
+        </Box>
+    );
+};
+
+export default ContactDashboardSection;
