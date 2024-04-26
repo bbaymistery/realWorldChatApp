aef4c9e3a80ee3fb846dde21b165dddb130aa4fd
bbaymistery <elgun.ezmemmedov@mail.ru>
2024-04-26 20:25:47 +0400

    We Have SET socket server.js and client App.js

--- cleint/src/App.js
+++ cleint/src/App.js
@@ -4,8 +4,13 @@ import Snackbar from "@mui/material/Snackbar";
 import MuiAlert from "@mui/material/Alert";
 import ThemeSettings from './components/settings';
 import { useDispatch, useSelector } from "react-redux";
-import React from "react";
+import React, { useEffect } from "react";
 import { closeSnackBar } from "./redux/slices/app";
+import openSocket from "socket.io-client";
+
+const socket = openSocket("http://localhost:3001",{
+  query: { user_id: "662ac2df1d3d69c4ed9b6d19" }
+});
 
 const vertical = "bottom";
 const horizontal = "center";
@@ -14,7 +19,19 @@ const Alert = React.forwardRef((props, ref) => <MuiAlert elevation={6} ref={ref}
 
 function App() {
   const dispatch = useDispatch();
-  const { severity="", message, open } = useSelector((state) => state.app.snackbar);
+  const { severity = "", message, open } = useSelector((state) => state.app.snackbar);
+  useEffect(() => {
+    // Setting up listeners for different socket events
+    socket.on("new_friend_request", data => {
+      console.log("Received a new friend request:", data);
+      // You can dispatch some action here to show notification or handle this event
+    });
+
+    // Cleanup on component unmount
+    return () => {
+      socket.off("new_friend_request");
+    };
+  }, []);
 
   return (
     <>
--- server/routes/user.js
+++ server/routes/user.js
@@ -5,6 +5,7 @@ const authController = require("../controllers/authController");
 //!User Controllers are  protected 
 
 router.patch("/update-me", authController.protect, userController.updateMe);
+router.post("/get-users", authController.protect, userController.getUsers);
 
 
 module.exports = router;
