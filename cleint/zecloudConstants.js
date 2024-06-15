aece9132c35cb84d55b1ff0093be120dd75bc28d
bbaymistery <elgun.ezmemmedov@mail.ru>
2024-06-15 16:11:09 +0400

    voice call ze cloud explained with video

---
+++ cleint/zecloudConstants.js
@@ -0,0 +1,6 @@
+export const zeCloudAppId=899260320
+export const zeCloudServerSecret="29f3f809441e10ec12a6a0cf6de4c773"
+export const zeCloudAppSign="c1e2e85d3de1df91ad2b05b53a62831feb2e901d95bbc157b92d1613b69dd959"
+export const zeCloudCallbackSecret="c1e2e85d3de1df91ad2b05b53a62831f"
+export const serverUrl1="wss://webliveroom899260320-api.coolzcloud.com/ws"
+export const SecondaryWebSocketServerAddress="wss://webliveroom899260320-api-bak.coolzcloud.com/ws"
