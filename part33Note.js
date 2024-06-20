9c13edd8c9c5dc2b07ccadb4dced52c5470bc20a
bbaymistery <elgun.ezmemmedov@mail.ru>
2024-06-20 15:48:42 +0400

    part34 yapilmadi nota  bak

--- part33Note.js
+++
@@ -1,5 +0,0 @@
-// https://www.youtube.com/watch?v=Q2ticoUH470&list=PLdLUE-L26MMbXYkddCi6Cb1jy5dKczosk&index=36   >link  
-// Bura geder one to one mesage hamsi ishdyr Burdan o terefe ise bilmirem nece olacag cunki elimdeki compda voice call video call yoxdu 
-//Sadece implementasyonu edeceyik Isliyecekmi ta onu buomirem Yani bir diger videonu srf ashina olum deye baxacam Meselen ZegoExpressEngine   falan  kullanacak front terefde 
-
-//Yucardaki partda ise logikani basa saldi 7 deygelik videodur
